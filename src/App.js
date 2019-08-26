b1e20624285d04220bb0386ba82ff3548156cc90
vcostap <viniciuscostapereira@hotmail.com>
2019-08-26 18:38:21 -0300

    V2.0
    Resulvido o problema da procura por personagens

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 /*Importando os componentes */
-import Header from './components/Header/header';
 import Titulo from './components/Titulo/titulo';
-import CardList from './components/CardList/cardList';
+import Card from './components/Card/card';
+
+import logo from './assets/logo.png';
 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      api: [],
+      inputSearch: ''
+    }
+    this.onChangeSearch = this.onChangeSearch.bind(this);
+  }
+
+  async componentDidMount() {
+    const url = "https://breakingbadapi.com/api/characters/";
+
+    const result = await axios.get(url)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((error) => {
+        return error;
+      })
+
+    const cards = result;
+    const api = result;
+    this.setState({
+      cards,
+      api
+    })
+  }
+
+  onChangeSearch(event) {
+    const { api } = this.state;
+    const arraySearch = api.filter(function (characters) {
+      const inputSearch = event.target.value.toLowerCase();
+      if (inputSearch !== '') {
+        return characters.name.toLowerCase().indexOf(inputSearch) > -1;
+      } else {
+        return api
+      }
+    });
+
+    const cards = arraySearch;
+    this.setState({
+      cards
+    })
+  }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Titulo />
-        <CardList />
+        <div className='container-principal'>
+          <header>
+            <img src={logo} alt="Breaking Bad logo"/>
+            <input 
+              type="search"
+              placeholder="Pesquise os personagens"
+              onChange={this.onChangeSearch}
+            />
+          </header>
+
+          <Titulo />
+          
+          <div className='conteudo'>
+            {
+              this.state.cards.map((personagem) => (
+                <Card personagem={personagem} />
+              ))
+            }
+          </div>
+        </div>
       </div>
     );
   }  
